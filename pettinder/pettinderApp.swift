2e41929803e950128f716f35a813d4f10a6024ae
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2024-04-24 12:39:38 -0500

    Initial Commit

---
+++ pettinder/pettinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  pettinderApp.swift
+//  pettinder
+//
+//  Created by Teodoro Calle Lara on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct pettinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pettinderUITests/pettinderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pettinderUITestsLaunchTests.swift
+//  pettinderUITests
+//
+//  Created by Teodoro Calle Lara on 24/04/24.
+//
+
+import XCTest
+
+final class pettinderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
